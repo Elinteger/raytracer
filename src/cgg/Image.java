9c89ed0888be11852c4384946e3d845b8778d498
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-23 21:11:44 +0100

    add code supplied by professor

---
+++ src/cgg/Image.java
@@ -0,0 +1,24 @@
+/** @author henrik.tramberend@beuth-hochschule.de */
+package cgg;
+
+import cgtools.*;
+
+public class Image {
+  public Image(int width, int height) {
+    notYetImplemented();
+  }
+
+  public void setPixel(int x, int y, Color color) {
+    notYetImplemented();
+  }
+
+  public void write(String filename) {
+    // Use cggtools.ImageWriter.write() to implement this.
+    notYetImplemented();
+  }
+
+  private void notYetImplemented() {
+    System.err.println("\nPlease complete the implementation of class cgg.Image as part of assignment 1.\n");
+    System.exit(1);
+  }
+}
